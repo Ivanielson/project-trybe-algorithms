3c766197c85299742b79a894c0d8f498a4c65afa
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-07-01 15:09:25 -0300

    :sparkles: feat: Implements study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not target_time:
+        return None
+    count = 0
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if target_time >= start and target_time <= end:
+            count += 1
+    return count
