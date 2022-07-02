42a21dc8a85bf98911bcfb30cfa98a5c08780619
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-07-02 10:40:39 -0300

    :recycle: refactor: remove table test

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,8 +7,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] != word[high_index]:
         return False
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
-
-if __name__ == "__main__":
-    word = "I"
-    print(is_palindrome_recursive(word, 0, len(word)-1))
