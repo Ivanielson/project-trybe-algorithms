9ca842fa04db94e38b5fd97c4e70d40757c17005
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-07-02 10:39:02 -0300

    :sparkles: feat: Implements is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if high_index < 0:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+if __name__ == "__main__":
+    word = "I"
+    print(is_palindrome_recursive(word, 0, len(word)-1))
